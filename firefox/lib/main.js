acd5f6c4d3db687316f1805fc940de2fa6381c5c
T. Scott Barnes <barnes.t.scott@gmail.com>
2013-11-20 13:26:38 -0500

    Change Firefox addon to use 'ready' event

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,9 +1,9 @@
 var self = require("sdk/self");
 var tabs = require("sdk/tabs");
 
-tabs.on('load', function(tab) {
-  if(tab.url.match(/.*:\/\/.*\.amazon\.co.*\/.*/)) {
-    if(!tab.url.match(/.*:\/\/smile\.amazon\.co.*\/.*/)) {
+tabs.on('ready', function(tab) {
+  if(tab.url.match(/.*:\/\/.*\.amazon\.com\/.*/)) {
+    if(!tab.url.match(/.*:\/\/smile\.amazon\.com\/.*/)) {
       tab.url = tab.url.replace(/:\/\/.*\.amazon/, '://smile.amazon');
     }
   }
