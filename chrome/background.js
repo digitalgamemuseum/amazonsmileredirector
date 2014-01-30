1d4d59b2e42892c116645c8cdaf54c4c1a57a9a1
T. Scott Barnes <barnes.t.scott@gmail.com>
2014-01-30 10:48:42 -0500

    Fixed typo in Chrome background script

--- chrome/background.js
+++ chrome/background.js
@@ -3,20 +3,15 @@
  */
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-  var redirectTo = details.url.replace(/:\/\/.*\.amazon/, '://smile.amazon');
-  console.log(details);
-  console.log(redirectTo);
-  return {
-    redirectUrl: redirectTo
-  };
+	var redirectTo = details.url.replace(/:\/\/.*\.amazon/, '://smile.amazon');
+	console.log(details);
+	console.log(redirectTo);
+	return {
+		redirectUrl: redirectTo
+	};
 }, {
-  urls: [
-  	"*://amazon.com/",
-  	"*://amazon.com/*/product/*",
-  	"*://amazon.com/s/*",
-  	"*://www.amazon.com/"
-  	"*://www.amazon.com/*/product/*",
-  	"*://www.amazon.com/s/*",
-  ],
-  types: ["main_frame"]
+	urls: ["*://amazon.com/", "*://amazon.com/*/product/*",
+			"*://amazon.com/s/*", "*://www.amazon.com/",
+			"*://www.amazon.com/*/product/*", "*://www.amazon.com/s/*"],
+	types: ["main_frame"]
 }, ['blocking']);
